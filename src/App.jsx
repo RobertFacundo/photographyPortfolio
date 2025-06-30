06b46a8e49d0e3b92122dcf855e18267fee22965
Facundo Robert <robert.facundo@hotmail.com>
2025-06-30 13:05:12 -0300

    !

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,34 @@ import { Footer } from "./components/Footer";
 import NavBar from "./components/NavBar";
 import './App.css';
 
-function App() {
+
+const AppContent = () => {
   const location = useLocation()
+  return (
+    <>
+      <Routes>
+        {/*
+          These routes will include the NavBar directly within their element prop.
+          If NavBar should always appear with these, this is fine.
+          If NavBar should be part of a larger layout, consider a Layout component.
+        */}
+        <Route path='/Home' element={<React.Fragment><NavBar /><Home /></React.Fragment>} />
+        <Route path='/Portraits' element={<React.Fragment><NavBar /><Portraits /></React.Fragment>} />
+        <Route path='/Places' element={<React.Fragment><NavBar /><Places /></React.Fragment>} />
+        <Route path='/Contact' element={<React.Fragment><NavBar /><Contact /></React.Fragment>} />
+      </Routes>
+      {/* Conditionally render the Footer based on the current path */}
+      {location.pathname !== '/' && <Footer />}</>
+  )
+}
+
+function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<SplashScreen />} />
-        <Route path="/Home" element={<React.Fragment><NavBar /><Home /></React.Fragment>} />
-        <Route path="/Portraits" element={<React.Fragment><NavBar /><Portraits /></React.Fragment>} />
-        <Route path="/Places" element={<React.Fragment><NavBar /><Places /></React.Fragment>} />
-        <Route path="/Contact" element={<React.Fragment><NavBar /><Contact /></React.Fragment>} />
+        <Router path='/*' element={<AppContent />} />
       </Routes>
-      {location.pathname !== '/' && <Footer />}
     </Router>
   );
 }
